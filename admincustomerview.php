6d7778b9cc0eb81eb37f162605255c377a02bc7d
ranuka98 <69889092+ranuka98@users.noreply.github.com>
2021-08-19 12:31:29 +0530

    responsive changes

--- admincustomerview.php
+++ admincustomerview.php
@@ -17,7 +17,7 @@
         <div class="nav-cont">
             <div class="admin-nav">
                 <ul>
-                    
+
                     <li>
                         <a href="adminrestaurantview.php">
                             <span class="material-icons">
@@ -35,21 +35,30 @@
                         </a>
                     </li>
                     <li>
-                    <form action="adminprofile.php" method="POST">
-							<p> <a href="adminprofile.php">View Foods</a> </p>	
-							</form>   
+
+                        <a href="adminprofile.php">
+                            <span class="material-icons">
+                                face
+                            </span>
+                            View Profile
+
+                        </a>
+                    </li>
+
+                    <li>
+
+                        <a href="login.php?logout='1'">
+                            <span class="material-icons">
+                                backspace
+                            </span>
+                            LOGOUT
+
+                        </a>
                     </li>
 
 
                 </ul>
-                <a href="login.php?logout='1'">
-                    <button>
-                        <span class="material-icons">
-                            backspace
-                        </span>
-                        LOGOUT
-                    </button>
-                </a>
+
             </div>
         </div>
         <div class="space-content">
