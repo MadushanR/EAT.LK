35bcffc6884a65c2dd63c2254eec2fbedf2b3ecc
ranuka98 <69889092+ranuka98@users.noreply.github.com>
2021-08-24 00:07:02 +0530

    added background image

---
+++ aboutus.php
@@ -0,0 +1,317 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="./assets/img/favicon.ico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./assets/img/favicon.ico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./assets/img/favicon.ico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/favicon.ico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./assets/img/favicon.ico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./assets/img/favicon.ico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./assets/img/favicon.ico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./assets/img/favicon.ico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/img/favicon.ico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="./assets/img/favicon.ico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon.ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./assets/img/favicon.ico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon.ico/favicon-16x16.png">
+    <link rel="stylesheet" type="text/css" href="./assets/js/vendor/owlcarasole/dist/assets/owl.carousel.min.css" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Smokingdots</title>
+
+    <!-- Icons for this template -->
+    <link href="assets/fonts/font-awesome/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Stylesheet -->
+    <!-- build:css -->
+    <link href="assets/css/app.css" rel="stylesheet">
+    <!-- endbuild -->
+</head>
+
+<body>
+
+    <main>
+
+        <!-- ./aboutUs section -->
+        <div class="aboutUs">
+
+            <div class="container">
+                <div class="aboutUs__header">
+                    <div class="aboutUs__title">
+                        The right software development partner can change everything
+                    </div>
+                    <div class="aboutUs__subTitle">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore e
+                    </div>
+                </div>
+                <div class="aboutUs__teamImg">
+                    <div class="aboutUs__teamCard card1">
+                        <img src="./assets/img/teamImages/image-4.png" alt="">
+                    </div>
+                    <div class="aboutUs__teamCard card2">
+                        <img src="./assets/img/teamImages/image-3.png" alt="">
+                    </div>
+                    <div class="aboutUs__teamCard card3">
+                        <img src="./assets/img/teamImages/image-1.png" alt="">
+                    </div>
+                    <div class="aboutUs__teamCard card4">
+                        <img src="./assets/img/teamImages/image-2.png" alt="">
+                    </div>
+                </div>
+                <div class="aboutUs__how">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="aboutUs__howTitle">
+                                How we work
+                            </div>
+                            <div class="aboutUs__howSubTitle">
+                                We focus on helping out all startup founders,who may have no technical knowledge on how
+                                they
+                                should build products to set out with, <br>
+                                Businesses that may have the best product ideas but have no way in setting out in making
+                                that a reality, All SME companies that would be going out to the extent of exceeding
+                                their
+                                development capacity and want to look into engaging with outside vendors.
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="aboutUs__howTitle">
+                                Our philosophy
+                            </div>
+                            <div class="aboutUs__howSubTitle">
+                                We set out to help all existing to newly created businesses, in ensuring that the
+                                business
+                                stays afloat. Peeking sky high is the dream of every businessman, every entrepreneur,
+                                but we
+                                do understand that it may not be easy, This is where we come in.We ensure that we do
+                                everything in our power and guidance to assist you so that you would have nothing, but
+                                smooth sailing.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="aboutUs__meetTeam">
+                    <div class="title__large">
+                        Meet our team
+                    </div>
+                    <div class="title__largeSub">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                    </div>
+                    <div class="aboutUs__ourTeam">
+                        <img src="./assets/img/teamImages/teamGroup.png" alt="">
+                    </div>
+                    <div class="aboutUs__teamSub">
+                        Lorem Ipsum is simply dummy and typesetting industry. LoremLorem Ipsum is simply dummy and
+                        typesetting industry. Lorem
+                    </div>
+
+                    <div class="teamMatesGrid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="teamMatesCard">
+                                    <div class="teamMatesCard__profile yellow">
+                                        <img src="./assets/img/teamImages/avindu.png" alt="">
+                                    </div>
+                                    <div class="teamMatesCard__name">
+                                        Avindu Hewa
+                                    </div>
+                                    <div class="teamMatesCard__postion">
+                                        Chief Executive Officer
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="teamMatesCard">
+                                    <div class="teamMatesCard__profile blue">
+                                        <img src="./assets/img/teamImages/sahan.png" alt="">
+                                    </div>
+                                    <div class="teamMatesCard__name">
+                                        Sahan Weerakoon
+                                    </div>
+                                    <div class="teamMatesCard__postion">
+                                        Software Engineer
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="teamMatesCard">
+                                    <div class="teamMatesCard__profile pink">
+                                        <img src="./assets/img/teamImages/step.png" alt="">
+                                    </div>
+                                    <div class="teamMatesCard__name">
+                                        Stefan Pereira
+                                    </div>
+                                    <div class="teamMatesCard__postion">
+                                        QA Engineer
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="teamMatesCard">
+                                    <div class="teamMatesCard__profile red">
+                                        <img src="./assets/img/teamImages/ranuka.png" alt="">
+                                    </div>
+                                    <div class="teamMatesCard__name">
+                                        Ranuka Perera
+                                    </div>
+                                    <div class="teamMatesCard__postion">
+                                        Associate Software Engineer
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="teamMatesCard">
+                                    <div class="teamMatesCard__profile green">
+                                        <img src="./assets/img/teamImages/waleed.png" alt="">
+                                    </div>
+                                    <div class="teamMatesCard__name">
+                                        Waleed Uzaim
+                                    </div>
+                                    <div class="teamMatesCard__postion">
+                                        Junior Accountant and HR Executive
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="teamMatesCard">
+                                    <div class="teamMatesCard__profile red">
+                                        <img src="./assets/img/teamImages/angie.png" alt="">
+                                    </div>
+                                    <div class="teamMatesCard__name">
+                                        Angie Hewa
+                                    </div>
+                                    <div class="teamMatesCard__postion">
+                                        Intern (Design team)
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ./aboutUs section -->
+
+    </main>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="footer__leftCorner">
+                        <div class="footer__logo">
+                            <img src="./assets/img/logo/logoblack.svg" alt="">
+                        </div>
+                        <div class="footer__title">
+                            em Ipsum is simply dummy and typesetti
+                        </div>
+                        <div class="footer__socialmedia">
+                            <div class="footer__socialMediaCard">
+                                <a href=""><i class="fab fa-behance"></i></a>
+                            </div>
+                            <div class="footer__socialMediaCard">
+                                <a href="#"> <i class="fab fa-facebook"></i></a>
+                            </div>
+                            <div class="footer__socialMediaCard">
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                            <div class="footer__socialMediaCard">
+                                <a href="#"><i class="fab fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="footer__itemSection">
+                        <div class="footer__itemTitle">
+                            Work
+                        </div>
+                        <div class="footer__itemDes">
+                            <a href="">All projects</a>
+                            <a href="">Projects</a>
+                            <a href="">Services</a>
+                            <a href="">Work</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="footer__itemSection">
+                        <div class="footer__itemTitle">
+                            Compay
+                        </div>
+                        <div class="footer__itemDes">
+                            <a href="">How we work</a>
+                            <a href="">Our process</a>
+                            <a href="">Our team</a>
+                            <a href="">Partners</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="footer__itemSection">
+                        <div class="footer__itemTitle">
+                            Services
+                        </div>
+                        <div class="footer__itemDes">
+                            <a href="">How we work</a>
+                            <a href="">Our process</a>
+                            <a href="">Careers</a>
+                            <a href="">Partners</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__bottom row">
+                <div class="footer__bottomLeft col-md-6">
+                    <a href="">How we work</a>
+                    <a href="">Our process</a>
+                    <a href="">Careers</a>
+                    <a href="">Partners</a>
+                </div>
+                <div class="footer__bottomRight col-md-6">
+                    © 2021 Copyright SmokinngDots • All Rights Reserved
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/vendor/jquery.slim.min.js"></script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <!-- <script src="assets/js/vendor/bootstrap.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/ScrollToPlugin.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/ScrollTrigger.min.js"></script>
+    <script src="./assets/js/vendor/owlcarasole/dist/owl.carousel.min.js"></script>
+
+    <!-- build:js -->
+    <script src="assets/js/app.js"></script>
+    <script src="assets/js/main.js"></script>
+    <!-- endbuild -->
+
+    <script>
+
+
+    </script>
+</body>
+
+</html>
